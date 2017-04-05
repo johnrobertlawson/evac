957604533b0370214e386c9007ab3f70a70af781
John Lawson <johnroblawson@gmail.com>
2017-04-05 11:40:41 -0500

    Updates docs

--- evac/utils/__init__.py
+++ evac/utils/__init__.py
@@ -1,11 +1,14 @@
-from .defaults import *
-from .evac_numbers import *
-from .evac_types import *
-from .exceptions import *
+""" Add any *_tools modules to utils namespace
+to make it easier to import.
+"""
+# from .defaults import *
+# from .evac_numbers import *
+# from .evac_types import *
+# from .exceptions import *
 from .fractal_tools import *
-from .get_data import *
+# from .get_data import *
 from .gis_tools import *
-from .met_constants import *
+# from .met_constants import *
 from .misc_tools import *
 from .reproject_tools import *
 from .unix_tools import *
--- evac/utils/fractal_tools.py
+++ evac/utils/fractal_tools.py
@@ -9,6 +9,7 @@ import scipy.stats as SS
 
 ### 1D PLOT ###
 
+"""
 ### VARIABLES ###
 R = 0 # Function, varying with scale t
 dR = 0 # height of pulse - rainfatll intensity increment
@@ -29,7 +30,7 @@ rho_i = 1 # minimum resolution or inner scale - in pixels
 rho_0 = 1200 # outer scale
 n = 1.6e5 # number of pulses
 # Enforce L/l between 3 and 6
-
+"""
 ### TEST DISTRUBTION ###
 # fig,ax = plt.subplots(1)
 # a_ = rho_i + 1
@@ -126,11 +127,6 @@ def fractal_worker(size=False,high=False,rholist=False,xx=False,
     q.put(R)
 
 
-fig8 = 1
-import time
-start = time.time()
-if not fig8:
-    raise Exception
 if __name__ == '__main__':
     CPUs = multiprocessing.cpu_count()-1
     rho_i = 6
@@ -162,8 +158,6 @@ if __name__ == '__main__':
     print("Starting computation now")
     # paralellise the processing
     npart = int(n/CPUs)
-    # centrex = N.random.random_integers(low=0,high=L,size=n)
-    # centrey = N.random.random_integers(low=0,high=L,size=n)
     Rs = []
     Rsum = N.zeros_like(R)
 
@@ -196,81 +190,4 @@ if __name__ == '__main__':
         Rsum += Rq
     for job in jobs:
         job.join()
-    # for cpu in CPUs:
-        # pool.apply(fractal_worker,args=(*listargs))
-        # pool.close()
-        # pool.join()
-        # Rs.append(fractal_worker())
-    # pdb.set_trace()
-    R = Rsum
-    fig,axes = plt.subplots(2,figsize=(12,12))
-    # fig,ax = plt.subplots(1,figsize=(12,10))
-    # R += -(0.70*R.max())
-    # R[R<0] = 1e-5
-    # R[R>100] = 100
-    # R = N.log(R)
-    # R[R<0] = 1e-5
-    # ISNAN = N.isnan(R)
-    # R[ISNAN] = 0
-    # R = R[int(l/2):-int(l/2),int(l/2):-int(l/2))
-    # xx, yy = N.mgrid[:l,:l]
-    ax = axes.flat[0]
-    cc = ax.pcolormesh(xx,yy,R,cmap=M.cm.Greys_r,)
-    ax.set_aspect('equal')
-    # cc = ax.pcolormesh(xx,yy,R,cmap=M.cm.Greys_r,norm=M.colors.LogNorm(vmin=R.max(),vmax=R.max()))
-    ax = axes.flat[1]
-    # vals, edges = N.histogram(R,bins=20)
-    ax.hist(R.flatten(),100)
-    fig.colorbar(cc)
-    fig.show()
-    fig.savefig('lovejoy_example.png')
-end = time.time()
-all_elapsed = "Total time: {0:.1f}".format(end-start)
-print(all_elapsed)
-assert True==False
-
-pulseshape = dR * N.exp(-1*(u/rho)**(2*s))
-pulselength = 0 # probability of rhotick > rho is 1/rho, for rho > 1
-rbar(cc)
-
-
-
-# 2D
-A = 0 # random area of pulse
-a = 0 # pulse area
-RT = 0 # threshold
-coverage = 0.3 # enforce coverage of rainfall/clouds
-
-
-# enforce values < 0 = 0
-Rtick = R-RT # rainrate
-Rtick = 0
-
-field = N.zeros([L,L])
-centre = N.zeros([l,l])
-
-
-dR = N.random.randint(-1,1) * A**H
-
-# best pulse shape
-LAMB = 1.2 # Radius
-radius = N.pi
-LAMBstar = N.sqrt(LAMB**2 - 1) # ?
-delta = 0.5*(LAMBstar + LAMB)
-sigma = 0.5*(LAMB-LAMBstar)
-pulseshape = dR * N.exp(-1*((u**2/rhotick**2)-delta**2)/sigma**2)**(2*s)
-# This should give smooth annulus of size rhotick.
-
-# A is set so that Pr( A greater than a) varies as 1/a
-ppcens = get_poissons()
-for ppcen in ppcens:
-    px, py = ppcen
-
-    field[px,py] = 0
-# fractal = field
-rate = field-RT
-
-fig,ax = plt.subplots(1)
-ax.pcolormesh(rate,cmap=M.cm.Greys)
-fig.colorbar
-fig.show()
+    return Rsum
