f4c9e9320b79ddda38182c1994e87669e91a96a0
John R. Lawson <johnroblawson@gmail.com>
2018-03-17 17:14:32 -0500

    New lazyWRF class

---
+++ evac/lazy/lazy_ensemble.py
@@ -0,0 +1,13 @@
+import os
+
+from evac.laz
+
+class LazyEnsemble:
+    """
+    Generate ensemble by running multiple WRF runs simulataneously.
+    This is done by creating temporary folder and linking/copying.
+    Efficiency improvements from Monte Flora.
+    """
+    def __init__(self,):
+        pass
+    
