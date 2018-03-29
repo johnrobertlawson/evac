2360f1ff69e3aa37cab874027d3eefdafe605c91
John Lawson <johnroblawson@gmail.com>
2018-03-29 11:38:07 -0500

    Updates docs

--- evac/datafiles/binaryfile.py
+++ evac/datafiles/binaryfile.py
@@ -1,7 +1,14 @@
-""" Any miscellaneous binary file.
-"""
 from .datafile import DataFile
 
 class BinaryFile(DataFile):
-    def __init__(self):
+    def __init__(self,fpath):
+        """ Superclass for binary files.
+
+        Args:
+            fpath: Absolute path to binary file. 
+
+        Todo:
+            * Nothing yet
+            * Not using MRMS yet
+        """
         super().__init__(fpath)
