16111f695a7a93d3675ed52be66894a5554963de
John Lawson <johnroblawson@gmail.com>
2018-03-29 12:02:16 -0500

    Updates docs

--- evac/datafiles/datafile.py
+++ evac/datafiles/datafile.py
@@ -9,8 +9,11 @@ class DataFile:
     Todo:
         * This is where derived functions should be attached
             (computing vorticity, etc) that are in :func:`~evac.derived.derived`.
+        * Also, see :func:`evac.derived.derived`.
         * Also, see :any:`~evac.derived.derived`.
+        * Also, see :any:`evac.derived.derived`.
         * Also, see :method:`~evac.derived.derived`.
+        * Also, see :method:`evac.derived.derived`.
     """
     def __init__(self,fpath):
         self.fpath = fpath
