0463a6ee7f6a7b238fbaa693b0c197c1c2db07a7
John Lawson <johnroblawson@gmail.com>
2018-03-29 14:38:25 -0500

    Updates docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
+    'sphinx.ext.mathbase',
     'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
