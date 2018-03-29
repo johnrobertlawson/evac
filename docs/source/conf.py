afedc0c19e53384317bdd44c5e18e0bd5c349b9b
John Lawson <johnroblawson@gmail.com>
2018-03-29 13:13:00 -0500

    Updates docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,6 +109,7 @@ html_theme = 'haiku'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = ['../images/evac_logo.svg']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
