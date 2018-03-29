577d603558ea8009ba9d7863ea952d97a39f245b
John Lawson <johnroblawson@gmail.com>
2018-03-29 13:00:54 -0500

    Updates docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'agogo'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
