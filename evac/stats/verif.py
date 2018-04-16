8a2342db10514cf259254f810e99954df15560b9
John Lawson <johnroblawson@gmail.com>
2018-04-15 20:29:07 -0500

    bugfix

--- evac/stats/verif.py
+++ evac/stats/verif.py
@@ -271,6 +271,7 @@ class Verif:
                 codedict = {}
                 for chunk in itr:
                     fchr,dom,lv,vrbl = chunk
+                    code = _get_loop_code(chunk)
                     utc = self.lookup_validtime(fchr)
 
                     rpj_fpath_f = self.check_for_reproj(vrbl=vrbl,model='fcst',lv=lv,utc=fchr,
