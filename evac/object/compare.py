c28bfdc251a58feb0e48a9071bbb5eb810c9f473
John Lawson <johnroblawson@gmail.com>
2018-12-29 18:37:41 +0000

    New object folder and templates

---
+++ evac/object/compare.py
@@ -0,0 +1,6 @@
+""" Collection of functions that take arguments of ObjectID instances,
+and compute differences, mean objects, etc.
+"""
+
+def find_mean_object():
+    pass
---
+++ evac/object/objectid.py
@@ -0,0 +1,10 @@
+""" Identify objects from array.
+"""
+
+import pdb
+import os
+
+class ObjectID:
+    def __init__(self):
+        pass
+
