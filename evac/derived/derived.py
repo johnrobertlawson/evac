6eaa1f73a4c6563a27c78fc0353908fa4679ca57
John Lawson <johnroblawson@gmail.com>
2019-04-18 14:15:09 -0500

    Trying to fix UH bug

--- evac/derived/derived.py
+++ evac/derived/derived.py
@@ -977,7 +977,7 @@ def return_updraught_helicity(parent,tidx,lvidx,lonidx,latidx,other,z0=2000,z1=5
         utils.enforce_same_dimensions(u,v,w)
         xi = compute_vorticity(parent=parent,U=u,V=v)
 
-        dz = N.diff(z[:,zs+1,:,:])
+        dz = N.diff(z[:,zs,:,:])
         # Final UH computation:
         assert xi.ndim == 4
         UH = N.sum(xi*w*dz,axis=1)[0,:,:]
